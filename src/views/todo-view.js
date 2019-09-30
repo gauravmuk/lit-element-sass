612a5cea586a0b9560f2728eda9ad8adaee1eadf
Gaurav Nanda <gaurav.nanda@wingify.com>
2019-09-30 11:28:41 +0530

    Passing data out from the component using events

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -5,10 +5,13 @@ class TodoView extends LitElement {
   static get styles() {
 		return [Style1];
   }
+  onTodoViewClick() {
+    this.dispatchEvent(new CustomEvent('kick', {detail: {kicked: true}}));
+  }
   render() {
     return html`
       <button class="yellow">Hello World</button>
-      <p>todo-view</p>
+      <p @click="${this.onTodoViewClick}">todo-view</p>
     `;
   }
 }
